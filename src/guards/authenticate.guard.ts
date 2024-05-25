e4668a0e32a26f7dd825eceda8f3f2e5fb643670
TacioAntonio <tacioantonio10@gmail.com>
2024-05-25 15:25:30 -0300

    feat: added guards

---
+++ src/guards/authenticate.guard.ts
@@ -0,0 +1,34 @@
+import {
+  Injectable,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export abstract class AuthenticateGuard {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) throw new UnauthorizedException();
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.SECRET_KEY_SHA1,
+      });
+      request.user = payload;
+
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers['authorization']?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
